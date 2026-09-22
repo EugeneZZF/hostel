ebb72de77abfa23ca1db7f503ba75e36667aabb4
eugenezzf <genya_520@mail.ru>
2026-09-22 19:21:56 +0300

    Env enable + JWTService -v1.0

---
+++ src/main/java/io/github/eugenezzf/Hostel/config/EnvConfig.java
@@ -0,0 +1,12 @@
+package io.github.eugenezzf.Hostel.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvConfig {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    public static String get(String key){
+        return dotenv.get(key);
+    }
+
+}
--- src/main/java/io/github/eugenezzf/Hostel/service/JwtService.java
+++ src/main/java/io/github/eugenezzf/Hostel/service/JwtService.java
@@ -1,7 +1,37 @@
 package io.github.eugenezzf.Hostel.service;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.github.eugenezzf.Hostel.config.EnvConfig;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service 
 public class JwtService {   
 
-    private final String secretkey = "12351";
+    private final String secretKey = EnvConfig.get("JWT_SECRET");
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder()
+        .subject(userDetails.getUsername())
+        .issuedAt(new Date())
+        .expiration(
+            new Date(System.currentTimeMillis() + 1000 *60 * 60)
+        )
+        .signWith(getSignInKey())
+        .compact();
+    }
+
+    private SecretKey getSignInKey(){
+        return Keys.hmacShaKeyFor(
+            secretKey.getBytes(StandardCharsets.UTF_8)
+        );
+    }
 
 }
